ff9d754526f5e86767e092ec5f606f5df4d191c5
ThuyVu <thuyvv.hn@gmail.com>
2023-07-24 15:34:12 +0700

    [update] Optimize phpQueryPlugins

--- src/phpQuery.php
+++ src/phpQuery.php
@@ -7541,33 +7541,33 @@ abstract class phpQuery
 }
 
 /**
+ * Class phpQueryPlugins
  * Plugins static namespace class.
  *
- * @author Tobiasz Cudnik <tobiasz.cudnik/gmail.com>
+ * @author  Tobiasz Cudnik <tobiasz.cudnik/gmail.com>
  * @package phpQuery
- * @todo move plugin methods here (as statics)
+ * @TODO    move plugin methods here (as statics)
  */
-class phpQueryPlugins {
-	public function __call($method, $args) {
-		if (isset(phpQuery::$extendStaticMethods[$method])) {
-			$return = call_user_func_array(
-				phpQuery::$extendStaticMethods[$method],
-				$args
-			);
-		} else if (isset(phpQuery::$pluginsStaticMethods[$method])) {
-			$class = phpQuery::$pluginsStaticMethods[$method];
-			$realClass = "phpQueryPlugin_$class";
-			$return = call_user_func_array(
-				array($realClass, $method),
-				$args
-			);
-			return isset($return)
-				? $return
-				: $this;
-		} else
-			throw new Exception("Method '{$method}' doesnt exist");
-	}
+class phpQueryPlugins
+{
+    public function __call($method, $args)
+    {
+        if (isset(phpQuery::$extendStaticMethods[$method])) {
+            $return = call_user_func_array(phpQuery::$extendStaticMethods[$method], $args);
+        }
+        elseif (isset(phpQuery::$pluginsStaticMethods[$method])) {
+            $class = phpQuery::$pluginsStaticMethods[$method];
+            $realClass = "phpQueryPlugin_$class";
+            $return = call_user_func_array([$realClass, $method], $args);
+        }
+        else {
+            throw new Exception("Method '{$method}' doesnt exist");
+        }
+
+        return isset($return) ? $return : $this;
+    }
 }
+
 /**
  * Shortcut to phpQuery::pq($arg1, $context)
  * Chainable.
